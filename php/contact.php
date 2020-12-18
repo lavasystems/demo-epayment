d00193c87ae450949fedb44a075f77f4d692feca
Fadli Saad <fadlisaad@gmail.com>
2020-12-18 13:29:37 +0800

    Update
    Signed-off-by: Fadli Saad <fadlisaad@gmail.com>

--- php/contact.php
+++ php/contact.php
@@ -38,8 +38,7 @@ if(get_magic_quotes_gpc()) {
 // Enter the email address that you want to emails to be sent to.
 // Example $address = "joe.doe@yourdomain.com";
 
-//$address = "example@example.net";
-$address = "example@example.net";
+$address = "hafiz.razi@perlis.gov.my";
 
 
 // Configuration option.
