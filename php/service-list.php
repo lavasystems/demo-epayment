51fdea28a5ce92c6a7439041288c77dc25d85791
Fadli Saad <fadlisaad@gmail.com>
2021-04-27 15:46:58 +0800

    Add enable filter

--- php/service-list.php
+++ php/service-list.php
@@ -3,7 +3,7 @@ require ('conn.php');
 
 $agency_id = $_POST['agency_id'];
 
-$stmt = $pdo->prepare("SELECT code, name FROM services WHERE agency_id = :agency_id");
+$stmt = $pdo->prepare("SELECT code, name FROM services WHERE agency_id = :agency_id AND enabled = 1");
 $stmt->execute(['agency_id' => $agency_id]); 
 $rows = $stmt->fetchAll();
 
